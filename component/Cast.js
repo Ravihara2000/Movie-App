57b15e243c328d85c820fdc007ab9fbfbf564e74
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-11-04 20:26:00 +0530

    cast images styles done

--- component/Cast.js
+++ component/Cast.js
@@ -1,8 +1,9 @@
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity,Image } from 'react-native';
 import React from 'react';
 
 export default function Cast({ cast }) {
   let characterName = 'John Wick';
+  let personName = "michel johnson";
 
   return (
     <View style={styles.container}>
@@ -12,9 +13,18 @@ export default function Cast({ cast }) {
           cast.map((person, index) => {
             return (
               <TouchableOpacity key={index} style={styles.castItem}>
+                <View style={styles.castImg}>
+                    <Image source={require('../assets/jhnwik.jpg')}
+          style={{height:50,width:50,borderRadius:10}}/>
+                </View>
                 <Text style={styles.characterName}>
                   {characterName.length > 10 ? characterName.slice(0, 10) + '...' : characterName}
                 </Text>
+                <Text style={{color:'white',fontSize:14}}>
+                            {
+                                personName.length>10? personName.slice(0,10)+ '...':personName
+                            }
+                        </Text>
               </TouchableOpacity>
             );
           })}
@@ -43,4 +53,7 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 14,
   },
+  castImg:{
+    backgroundColor:'yellow',
+  }
 });
