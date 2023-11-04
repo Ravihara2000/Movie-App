6557e3d91f02f9802d8a984cfecc71cb8f3861d7
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-11-04 20:08:53 +0530

    cast names horizontal view issue fixed

--- component/Cast.js
+++ component/Cast.js
@@ -1,51 +1,46 @@
-import { View, Text,StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import React from 'react';
 
-export default function Cast({cast}) {
-    let personName = 'Keanu Reevs';
-    let characterName = 'John Wick';
+export default function Cast({ cast }) {
+  let characterName = 'John Wick';
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Top Cast</Text>
-      <ScrollView
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      contentContainerStyle={{paddingHorizontal:15}}/>
-        {
-            cast && cast.map((person, index)=>{
-                return(
-                    <TouchableOpacity
-                    key={index}
-                    style={{alignItems:'center',flexDirection:'row',}}>
-                        <Text style={{color:'white',fontSize:14}}>
-                            {
-                                characterName.length>10? characterName.slice(0,10)+ '...':characterName
-                            }
-                        </Text>
-                        <Text style={{color:'white',fontSize:14}}>
-                            {
-                                personName.length>10? personName.slice(0,10)+ '...':personName
-                            }
-                        </Text>
-
-                    </TouchableOpacity>
-                )
-            })
-        }
-        
-    <ScrollView/>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.scrollContent}>
+        {cast &&
+          cast.map((person, index) => {
+            return (
+              <TouchableOpacity key={index} style={styles.castItem}>
+                <Text style={styles.characterName}>
+                  {characterName.length > 10 ? characterName.slice(0, 10) + '...' : characterName}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+      </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor:'black',
-      marginTop:10,
-    },
-    title:{
-        color:'white',
-
-    }
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    marginTop: 10,
+  },
+  title: {
+    color: 'white',
+  },
+  scrollContent: {
+    paddingHorizontal: 15,
+  },
+  castItem: {
+    alignItems: 'center',
+    marginRight: 25, // Add some spacing between cast members
+  },
+  characterName: {
+    color: 'white',
+    fontSize: 14,
+  },
 });
