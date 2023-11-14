85c426d45890df1924729e631c4f751bf8edccb0
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-11-14 22:46:36 +0530

    search bar border design done

--- component/SearchScreen.js
+++ component/SearchScreen.js
@@ -1,18 +1,35 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function SearchScreen() {
   return (
-    <View style={styles.container}>
-      <Text>SearchScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder='Search Movies'
+          placeholderTextColor='grey'
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    }});
+  container: {
+    width: '100%',
+    height: 40,
+    backgroundColor: 'black', // Set the background color to black
+    borderRadius: 25,
+    paddingHorizontal: 16,
+    marginTop: 16,
+    borderWidth: 1, // Add a white border
+    borderColor: 'white', // Set the border color to white
+    justifyContent: 'center', // Center content vertically
+  },
+  searchInput: {
+    flex: 1,
+    color: 'white', // Set the text color to white
+  },
+});
