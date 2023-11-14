c24bfda346e06a4016f447fde6e888bbac33cd1a
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-11-14 23:02:25 +0530

    close icon added

--- component/SearchScreen.js
+++ component/SearchScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { useNavigation } from '@react-navigation/native';
+import { XMarkIcon } from 'react-native-heroicons/outline';
 export default function SearchScreen() {
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
       <View style={styles.container}>
@@ -11,6 +13,9 @@ export default function SearchScreen() {
           placeholder='Search Movies'
           placeholderTextColor='grey'
         />
+        <TouchableOpacity onPress={()=>navigation.navigate("Home")}>
+          <XMarkIcon size={25} color="white" style={{marginTop: 7,backgroundColor:"grey", borderRadius:50}}/>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -18,6 +23,7 @@ export default function SearchScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection:"row",
     width: '100%',
     height: 40,
     backgroundColor: 'black', // Set the background color to black
